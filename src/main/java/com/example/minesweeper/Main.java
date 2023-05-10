aad834305b4396d18b8eb4eb50afcaacbff95e38
Tuğçe <tugce.okcu@hotmail.com>
2023-05-10 19:05:56 +0300

    first

---
+++ src/main/java/com/example/minesweeper/Main.java
@@ -0,0 +1,11 @@
+package com.example.minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MineSweeper mineSweeper= new MineSweeper(4,3);
+        mineSweeper.start();
+
+    }
+
+}
