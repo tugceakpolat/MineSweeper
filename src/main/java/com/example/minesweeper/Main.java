f008d8840e33778e58a48c01d7857fee307572e1
Tuğçe <tugce.okcu@hotmail.com>
2023-05-12 22:32:32 +0300

    error

--- src/main/java/com/example/minesweeper/Main.java
+++ src/main/java/com/example/minesweeper/Main.java
@@ -17,7 +17,7 @@ import java.util.Scanner;
         board = new Cell[rows][column];
 
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
+            for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = new Cell();
             }
         }
@@ -28,8 +28,8 @@ import java.util.Scanner;
 
 
             if (!board[i][j].isHasBomb()) {
-                board[i][j].setHasBomb(true);
 
+                board[i][j].setHasBomb(true);
                 bombCount--;
 
             }
@@ -43,7 +43,8 @@ import java.util.Scanner;
         int bombNumber = 0;
 
         for(int i =0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
+            for(int j=0; j<board[i].length; j++){
+
                 if( board[i][j].getContent() == "-"){
                     unOpenedCells++;
                 }
@@ -53,7 +54,7 @@ import java.util.Scanner;
             }
         }
 
-        return unOpenedCells == bombNumber;
+        return  bombNumber == unOpenedCells ;
     }
 
     void userInput(){
@@ -71,32 +72,33 @@ import java.util.Scanner;
         boolean finish = false;
 
         while (!finish){
-            System.out.print("Please enter a rows :");
-            int newRows = input.nextInt() - 1;
+            System.out.print("Please enter a row :");
+            int newRows = input.nextInt()-1;
 
             System.out.print("Please enter a column : ");
-            int newColumn = input.nextInt() - 1;
-            printMap();
+            int newColumn = input.nextInt()-1;
 
             int bombCountOfCell = 0;
-            if((newRows > rows || newColumn > column) && (newRows < 0 || newColumn < 0) ){
+            if((newRows >= rows || newColumn >= column) || (newRows < 0 || newColumn < 0) ){
                 System.out.println("Row or column choice is out of bounds. Try again.");
-            } else if (board[newRows][newColumn].hasBomb) {
+            } else if (board[newRows][newColumn].isHasBomb()) {
                 System.out.println("Bomb exploded! You lose!");
                 printMineMap();
                 finish = true;
-            } else if (board[newRows][newColumn].getContent() != "-") {
+            }
+            else if (board[newRows][newColumn].getContent() != "-") {
                 System.out.println("This sell is already selected. Try another cell.");
             }
             else{
                 for(int i = newRows-1; i < newRows +2; i++){
                     for (int j = newColumn -1; j < newColumn +2; j++ ){
-                        if((i >= 0 && j >= 0) && (i < rows && j < column) && board[i][j].hasBomb ){
+                        if(i >= 0 && j >= 0 && i < rows && j < column && board[i][j].isHasBomb()){
                             bombCountOfCell++;
                         }
                     }
                 }
                 board[newRows][newColumn].setContent(Integer.toString(bombCountOfCell));
+                printMap();
 
                 if (checkWin()){
                     System.out.println("You win!");
@@ -110,7 +112,7 @@ import java.util.Scanner;
     void printMap(){
         System.out.println();
         for(int i = 0; i<board.length; i++){
-            for(int j = 0; j<board.length; j++){
+            for(int j = 0; j<board[i].length; j++){
                 System.out.print(board[i][j].getContent() + " ");
             }
             System.out.println();
@@ -119,21 +121,22 @@ import java.util.Scanner;
 
 
     }
-
     void printMineMap(){
-        System.out.println(" ");
+        System.out.println();
         for (int i = 0; i<board.length; i++){
-            for(int j = 0; j< board.length; j++){
-                if(board[i][j].hasBomb){
+            for(int j = 0; j< board[i].length; j++){
+                if(board[i][j].isHasBomb()){
                     System.out.print("* ");
                 }
                 else {
-                    System.out.print("- ");
+                    System.out.print(board[i][j].getContent() + " ");
                 }
-                System.out.println();
             }
             System.out.println();
         }
+        System.out.println();
+
+
 
     }
 
