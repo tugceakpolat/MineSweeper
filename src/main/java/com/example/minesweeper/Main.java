4ce059a95f0de8d9bcd85ede54b8edda0c815a9e
Tuğçe <tugce.okcu@hotmail.com>
2023-05-12 20:17:14 +0300

    class

--- src/main/java/com/example/minesweeper/Main.java
+++ src/main/java/com/example/minesweeper/Main.java
@@ -3,7 +3,7 @@ package com.example.minesweeper;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+  class MineSweeper{
 
     int rows;
     int column;
@@ -34,36 +34,36 @@ public class Main {
         }
     }
 
-        public boolean checkWin(){
+    public boolean checkWin(){
         int unOpenedCells = 0;
         int bombNumber = 0;
 
         for(int i =0; i<board.length; i++){
             for(int j=0; j<board.length; j++){
-               if( board[i][j].getContent() == "-"){
-                   unOpenedCells++;
-               }
-               if(board[i][j].isHasBomb()){
-                   bombNumber++;
-               }
+                if( board[i][j].getContent() == "-"){
+                    unOpenedCells++;
+                }
+                if(board[i][j].isHasBomb()){
+                    bombNumber++;
+                }
             }
         }
 
         return unOpenedCells == bombNumber;
-        }
+    }
 
-        public void userInput(){
+    public void userInput(){
 
-            System.out.println("Welcome to the mine sweeper!");
+        System.out.println("Welcome to the mine sweeper!");
 
-            System.out.print("Please enter a rows number : ");
-            rows = input.nextInt();
+        System.out.print("Please enter a rows number : ");
+        rows = input.nextInt();
 
-            System.out.print("Please enter a column number : ");
-            column = input.nextInt();
-            createBoard();
-        }
-        public void playGame(){
+        System.out.print("Please enter a column number : ");
+        column = input.nextInt();
+        createBoard();
+    }
+    public void playGame(){
         boolean finish = false;
 
         while (!finish){
@@ -98,40 +98,47 @@ public class Main {
                 }
             }
         }
-        }
+    }
 
-        public void printMap(){
-            System.out.println("1");
-            for(int i = 0; i<board.length; i++){
-                for(int j = 0; j<board.length; j++){
-                    System.out.println(board[i][j].getContent() + " ");
+    public void printMap(){
+        System.out.println("1");
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board.length; j++){
+                System.out.println(board[i][j].getContent() + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("2");
+
+
+    }
+
+    public void printMineMap(){
+        System.out.println(" ");
+        for (int i = 0; i<board.length; i++){
+            for(int j = 0; j< board.length; j++){
+                if(board[i][j].hasBomb){
+                    System.out.println("* ");
+                }
+                else {
+                    System.out.println("- ");
                 }
                 System.out.println();
             }
             System.out.println("2");
-
-
         }
 
-        public void printMineMap(){
-            System.out.println(" ");
-            for (int i = 0; i<board.length; i++){
-                for(int j = 0; j< board.length; j++){
-                    if(board[i][j].hasBomb){
-                        System.out.println("* ");
-                    }
-                    else {
-                        System.out.println("- ");
-                    }
-                    System.out.println();
-                }
-                System.out.println("2");
-            }
+    }
 
-        }
 
-    public void main(String[] args) {
-        userInput();
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MineSweeper mineSweeper = new MineSweeper();
+        mineSweeper.userInput();
     }
 
 
